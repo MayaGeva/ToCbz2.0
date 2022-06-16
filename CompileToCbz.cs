c7b74079850eff0a8fbc9a2e0e2874ad84908b13
MayaGeva <gevamaya54@gmail.com>
2022-06-16 18:42:24 +0300

    Initial commit

---
+++ CompileToCbz.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+
+namespace ToCbz2
+{
+    class CompileToCbz
+    {
+        public DirectoryInfo Path { get; set; }
+        public DirectoryInfo Destination { get; set; }
+        public int FileName { get; set; }
+        public CompileToCbz(string path)
+        {
+            Path = new DirectoryInfo(path);
+            Destination = new DirectoryInfo(path + "destination");
+            FileName = 1;
+            Compile();
+        }
+        public void Compile()
+        {
+            var folders = Path.EnumerateDirectories().OrderBy(d => d.CreationTime).ToArray();
+            Array.Reverse(folders);
+            Directory.CreateDirectory(Destination.FullName);
+            for (int i = 0; i < folders.Length; i++)
+            {
+                Convert(folders[i]);
+            }
+
+            Zip(Directory.GetParent(Directory.GetParent(Path.FullName).FullName));
+        }
+        public void Zip(DirectoryInfo endPath)
+        {
+            string zipPath = endPath.FullName + "\\" + Path.Name + ".zip";
+
+            ZipFile.CreateFromDirectory(Destination.FullName, zipPath);
+            File.Move(zipPath, zipPath.Replace(".zip", ".cbz"));
+            DeleteFiles(Directory.GetParent(Destination.FullName));
+        }
+        public void Convert(DirectoryInfo folder)
+        {
+            string addToNum = "";
+            FileInfo[] files = folder.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                if (FileName > 1000)
+                    addToNum = "";
+                else if (FileName > 100)
+                    addToNum = "0";
+                else if (FileName > 10)
+                    addToNum = "00";
+                else
+                    addToNum = "000";
+                File.Copy(file.FullName, Destination.FullName + "\\" + addToNum + FileName + ".png");
+                FileName++;
+            }
+            DeleteFiles(folder);
+        }
+        public void DeleteFiles(DirectoryInfo folder)
+        {
+            Directory.Delete(folder.FullName, true);
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ToCbz2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.ShowNewFolderButton = false;
+            fbd.Description = "Choose Manga Directory";
+            // Show the FolderBrowserDialog.
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                CompileToCbz cc = new CompileToCbz(fbd.SelectedPath + "\\");
+            }
+        }
+    }
+}
